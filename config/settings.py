f763c1a63e8c460acc73a38dcaa8e7485148d2ae
vasilij korobov <kavyabass51@gmail.com>
2025-04-27 20:37:47 +0700

    redis и другие дополнительные чувствительные данные вынесены в переменные окружения

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': os.getenv('LOCATION'),
     }
 }
